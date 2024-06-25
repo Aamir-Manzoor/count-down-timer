4c5968c686ecd6c8e83642ff0cd71db2119c52c7
aamir221 <aamirmanzoor372@gmail.com>
2024-06-26 00:00:52 +0530

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        flip: "flip 1s cubic-bezier(0, 0, 0.2, 1) infinite",
+      },
+      keyframes: {
+        flip: {
+          from: { transform: "rotateX(0deg)", transformOrigin: "50% bottom " },
+          to: { transform: "rotateX(180deg)", transformOrigin: "50% bottom " },
+        },
+      },
+    },
+    fontFamily: {
+      redhat: ["Red Hat Text", "sans-serif"],
     },
   },
   plugins: [],
