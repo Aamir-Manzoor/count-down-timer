55342c5b0f15269c7ccfdd7c8cefff86a3d68412
aamir221 <aamirmanzoor372@gmail.com>
2024-06-26 00:16:36 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,54 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+"use client"
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { TimerContainer } from './components/TimerContainer';
+
 
 const Home: NextPage = () => {
+  const [newTime, setNewTime] = useState<number>(25); // default time in minutes
+  const [minutes, setMinutes] = useState<number>(25);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [message, setMessage] = useState<string>('');
+  const [countDownDate, setCountDownDate] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (countDownDate === null) return;
+
+    const updateTime = setInterval(() => {
+      const now = new Date().getTime();
+      const difference = countDownDate - now;
+
+      const newMinutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const newSeconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      setMinutes(newMinutes);
+      setSeconds(newSeconds);
+
+      if (difference <= 0) {
+        clearInterval(updateTime);
+        setMessage('The Launch Has Started');
+        setMinutes(0);
+        setSeconds(0);
+      }
+    }, 1000);
+
+    return () => clearInterval(updateTime);
+  }, [countDownDate]);
+
+  const handleClick = () => {
+    const timeInMilliseconds = newTime * 60 * 1000;
+    setCountDownDate(new Date().getTime() + timeInMilliseconds);
+    setMessage('');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputTime = parseInt(e.target.value, 10);
+    setNewTime(inputTime);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
       <Head>
@@ -9,9 +56,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <p className="text-[white]">Hello</p>
+      <Header message={message} />
+
+      <TimerContainer minutes={minutes} seconds={seconds} />
+
+      <Footer />
     </div>
   );
 };
 
 export default Home;
+
